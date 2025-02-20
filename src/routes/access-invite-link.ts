3b2266aa2e0c7ca59422ded3c8db8cac99a694e0
Rodrigo Martins <mr.rodrigocarrico@gmail.com>
2025-02-20 09:39:19 +0000

    second class finished

--- src/routes/access-invite-link.ts
+++ src/routes/access-invite-link.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { env } from '../env'
+import { accessInviteLink } from '../functions/access-invite-link'
 
 export const accessInviteLinkRoute: FastifyPluginAsyncZod = async (app) => {
     app.get(
@@ -22,7 +23,8 @@ export const accessInviteLinkRoute: FastifyPluginAsyncZod = async (app) => {
     async (request, reply) => {
         const { subscriberId } = request.params
 
-        console.log(subscriberId)
+        await accessInviteLink({subscriberId})
+
 
         const redirectUrl = new URL(env.WEB_URL)
 
